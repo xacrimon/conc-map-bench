d12361f126baac7ed3817d79d310dd1cfbdfaefa
Acrimon <joel.wejdenstal@gmail.com>
2020-02-29 23:08:08 +0100

    Added insert benches.

--- benches/insert_heavy.rs
+++ benches/insert_heavy.rs
@@ -3,46 +3,46 @@ use conc_map_bench::{CHashMapTable, ContrieTable, FlurryTable, MutexStdTable, Rw
 
 fn main() {
     tracing_subscriber::fmt::init();
-    println!("Read heavy preset:");
-    println!("  Read: 94%");
-    println!("  Insert: 2%");
-    println!("  Update: 3%");
-    println!("  Remove: 1%");
+    println!("Insert heavy preset:");
+    println!("  Read: 10%");
+    println!("  Insert: 80%");
+    println!("  Update: 10%");
+    println!("  Remove: 0%");
     println!("  Upsert: 0%\n");
 
     println!("-- MutexStd");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<MutexStdTable<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<MutexStdTable<u64>>();
     }
     println!("");
 
     println!("-- RwLockStd");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<RwLockStdTable<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<RwLockStdTable<u64>>();
     }
     println!("");
 
     println!("-- CHashMap");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<CHashMapTable<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<CHashMapTable<u64>>();
     }
     println!("");
 
     println!("-- Contrie");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<ContrieTable<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<ContrieTable<u64>>();
     }
     println!("");
 
     println!("-- Flurry");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<FlurryTable<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<FlurryTable<u64>>();
     }
     println!("");
 
     println!("-- DashMapV3");
     for n in 1..=num_cpus::get() {
-        Workload::new(n, Mix::read_heavy()).run::<DashMapV3Table<u64>>();
+        Workload::new(n, Mix::insert_heavy()).run::<DashMapV3Table<u64>>();
     }
     println!("");
 }
