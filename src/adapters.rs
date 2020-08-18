e48964d638617c8c990d0819bfcfe4f9139248e4
Acrimon <joel.wejdenstal@gmail.com>
2020-08-19 01:04:29 +0200

    updated benches

--- src/adapters.rs
+++ src/adapters.rs
@@ -1,24 +1,21 @@
 use bustle::*;
-use chashmap::CHashMap;
-use contrie::ConMap;
 use dashmap::DashMap;
-use fxhash::FxBuildHasher;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use shard_lock::Shard;
+use parking_lot::RwLock;
 
 #[derive(Clone)]
-pub struct MutexStdTable<K>(Arc<Mutex<HashMap<K, u32, FxBuildHasher>>>);
+pub struct ShardTable<K>(Arc<Shard<RwLock<HashMap<K, u32>>>>);
 
-impl<K> Collection for MutexStdTable<K>
+impl<K> Collection for ShardTable<K>
 where
-    K: Send + From<u64> + Copy + 'static + std::hash::Hash + Eq,
+    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq,
 {
     type Handle = Self;
     fn with_capacity(capacity: usize) -> Self {
-        Self(Arc::new(Mutex::new(HashMap::with_capacity_and_hasher(
-            capacity,
-            FxBuildHasher::default(),
-        ))))
+        let map: Arc<Shard<RwLock<HashMap<K, u32>>>> = Arc::new(Shard::new(HashMap::with_capacity(capacity)));
+        Self(map)
     }
 
     fn pin(&self) -> Self::Handle {
@@ -26,166 +23,32 @@ where
     }
 }
 
-impl<K> CollectionHandle for MutexStdTable<K>
+impl<K> CollectionHandle for ShardTable<K>
 where
     K: Send + From<u64> + Copy + 'static + std::hash::Hash + Eq,
 {
     type Key = K;
 
     fn get(&mut self, key: &Self::Key) -> bool {
-        self.0.lock().unwrap().get(key).is_some()
+        self.0.read(key).get(key).is_some()
     }
 
     fn insert(&mut self, key: &Self::Key) -> bool {
-        self.0.lock().unwrap().insert(*key, 0).is_none()
+        self.0.write(key).insert(*key, 0).is_none()
     }
 
     fn remove(&mut self, key: &Self::Key) -> bool {
-        self.0.lock().unwrap().remove(key).is_some()
+        self.0.write(key).remove(key).is_some()
     }
 
     fn update(&mut self, key: &Self::Key) -> bool {
-        let mut map = self.0.lock().unwrap();
+        let mut map = self.0.write(key);
         map.get_mut(key).map(|v| *v += 1).is_some()
     }
 }
 
 #[derive(Clone)]
-pub struct CHashMapTable<K>(Arc<CHashMap<K, u32>>);
-
-impl<K> Collection for CHashMapTable<K>
-where
-    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
-{
-    type Handle = Self;
-    fn with_capacity(capacity: usize) -> Self {
-        Self(Arc::new(CHashMap::with_capacity(capacity)))
-    }
-
-    fn pin(&self) -> Self::Handle {
-        self.clone()
-    }
-}
-
-impl<K> CollectionHandle for CHashMapTable<K>
-where
-    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
-{
-    type Key = K;
-
-    fn get(&mut self, key: &Self::Key) -> bool {
-        self.0.get(key).is_some()
-    }
-
-    fn insert(&mut self, key: &Self::Key) -> bool {
-        self.0.insert(*key, 0).is_none()
-    }
-
-    fn remove(&mut self, key: &Self::Key) -> bool {
-        self.0.remove(key).is_some()
-    }
-
-    fn update(&mut self, key: &Self::Key) -> bool {
-        self.0
-            .get_mut(key)
-            .map(|mut r| {
-                *r += 1;
-            })
-            .is_some()
-    }
-}
-
-#[derive(Clone)]
-pub struct ContrieTable<K: Eq + std::hash::Hash + 'static>(Arc<ConMap<K, Mutex<u32>>>);
-
-impl<K> Collection for ContrieTable<K>
-where
-    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
-{
-    type Handle = Self;
-    fn with_capacity(_: usize) -> Self {
-        Self(Arc::new(ConMap::new()))
-    }
-
-    fn pin(&self) -> Self::Handle {
-        self.clone()
-    }
-}
-
-impl<K> CollectionHandle for ContrieTable<K>
-where
-    K: Send + Sync + From<u64> + Copy + 'static + std::hash::Hash + Eq + std::fmt::Debug,
-{
-    type Key = K;
-
-    fn get(&mut self, key: &Self::Key) -> bool {
-        self.0.get(key).is_some()
-    }
-
-    fn insert(&mut self, key: &Self::Key) -> bool {
-        self.0.insert(*key, Mutex::new(0)).is_none()
-    }
-
-    fn remove(&mut self, key: &Self::Key) -> bool {
-        self.0.remove(key).is_some()
-    }
-
-    fn update(&mut self, key: &Self::Key) -> bool {
-        self.0
-            .get(key)
-            .map(|e| {
-                *e.value().lock().unwrap() += 1;
-            })
-            .is_some()
-    }
-}
-
-#[derive(Clone)]
-pub struct FlurryTable(Arc<flurry::HashMap<u64, u32, FxBuildHasher>>);
-
-pub struct FlurryHandle(flurry::HashMapRef<'static, u64, u32, FxBuildHasher>);
-
-impl Collection for FlurryTable {
-    type Handle = FlurryHandle;
-
-    fn with_capacity(capacity: usize) -> Self {
-        Self(Arc::new(flurry::HashMap::with_capacity_and_hasher(
-            capacity,
-            FxBuildHasher::default(),
-        )))
-    }
-
-    fn pin(&self) -> Self::Handle {
-        unsafe {
-            std::mem::transmute(self.0.pin())
-        }
-    }
-}
-
-impl CollectionHandle for FlurryHandle {
-    type Key = u64;
-
-    fn get(&mut self, key: &Self::Key) -> bool {
-        self.0.get(key).is_some()
-    }
-
-    fn insert(&mut self, key: &Self::Key) -> bool {
-        self.0.insert(*key, 0).is_none()
-    }
-
-    fn remove(&mut self, key: &Self::Key) -> bool {
-        self.0.remove(key).is_some()
-    }
-
-    fn update(&mut self, key: &Self::Key) -> bool {
-        self.0
-            .compute_if_present(key, |_, v| Some(v + 1))
-            .is_some()
-    }
-}
-
-#[derive(Clone)]
-pub struct DashMapTable<K>(Arc<DashMap<K, u32, FxBuildHasher>>);
+pub struct DashMapTable<K>(Arc<DashMap<K, u32>>);
 
 impl<K> Collection for DashMapTable<K>
 where
@@ -193,10 +56,7 @@ where
 {
     type Handle = Self;
     fn with_capacity(capacity: usize) -> Self {
-        Self(Arc::new(DashMap::with_capacity_and_hasher(
-            capacity,
-            FxBuildHasher::default(),
-        )))
+        Self(Arc::new(DashMap::with_capacity(capacity)))
     }
 
     fn pin(&self) -> Self::Handle {
@@ -211,18 +71,18 @@ where
     type Key = K;
 
     fn get(&mut self, key: &Self::Key) -> bool {
-        self.0.extract(key, |_, _| ()).is_some()
+        self.0.get(key).is_some()
     }
 
     fn insert(&mut self, key: &Self::Key) -> bool {
-        self.0.insert(*key, 0) == false
+        self.0.insert(*key, 0).is_none()
     }
 
     fn remove(&mut self, key: &Self::Key) -> bool {
-        self.0.remove(key)
+        self.0.remove(key).is_some()
     }
 
     fn update(&mut self, key: &Self::Key) -> bool {
-        self.0.update(key, |_, v| v + 1)
+        self.0.get_mut(key).map(|mut v| *v += 1).is_some()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
 mod adapters;
 
-use adapters::{
-    CHashMapTable, ContrieTable, DashMapTable, FlurryTable,
-    MutexStdTable,
-};
+use adapters::{DashMapTable, ShardTable};
 use bustle::*;
-use std::thread::sleep;
-use std::time::Duration;
-
-#[global_allocator]
-static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
-
-fn gc_cycle() {
-    sleep(Duration::from_millis(20000));
-    let mut new_guard = crossbeam_epoch::pin();
-    new_guard.flush();
-    for _ in 0..32 {
-        new_guard.repin();
-    }
-    let mut old_guard = crossbeam_epoch_old::pin();
-    old_guard.flush();
-
-    for _ in 0..32 {
-        old_guard.repin();
-    }
-}
 
 fn read_heavy(n: usize) -> Workload {
     *Workload::new(n, Mix::read_heavy())
@@ -33,146 +10,22 @@ fn read_heavy(n: usize) -> Workload {
         .operations(1.5)
 }
 
-fn rg_mix() -> Mix {
-    Mix {
-        read: 5,
-        insert: 80,
-        remove: 5,
-        update: 10,
-        upsert: 0,
-    }
-}
-
-fn rapid_grow(n: usize) -> Workload {
-    *Workload::new(n, rg_mix())
-        .initial_capacity_log2(24)
-        .prefill_fraction(0.0)
-        .operations(1.5)
-}
-
-fn ex_mix() -> Mix {
-    Mix {
-        read: 10,
-        insert: 40,
-        remove: 40,
-        update: 10,
-        upsert: 0,
-    }
-}
-
-fn exchange(n: usize) -> Workload {
-    *Workload::new(n, ex_mix())
-        .initial_capacity_log2(24)
-        .prefill_fraction(0.8)
-        .operations(1.5)
-}
-
-fn exchange_task() {
-    println!("== exchange");
-    println!("-- MutexStd");
-    for n in 1..=num_cpus::get() {
-        exchange(n).run::<MutexStdTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- CHashMap");
-    for n in 1..=num_cpus::get() {
-        exchange(n).run::<CHashMapTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- Flurry");
-    for n in 1..=num_cpus::get() {
-        exchange(n).run::<FlurryTable>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- Contrie");
-    for n in 1..=num_cpus::get() {
-        exchange(n).run::<ContrieTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- DashMap");
-    for n in 1..=num_cpus::get() {
-        exchange(n).run::<DashMapTable<u64>>();
-        gc_cycle();
-    }
-    println!("==");
-}
-
 fn cache_task() {
-    println!("== cache");
-    println!("-- MutexStd");
-    for n in 1..=num_cpus::get() {
-        read_heavy(n).run::<MutexStdTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- CHashMap");
-    for n in 1..=num_cpus::get() {
-        read_heavy(n).run::<CHashMapTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- Flurry");
-    for n in 1..=num_cpus::get() {
-        read_heavy(n).run::<FlurryTable>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- Contrie");
-    for n in 1..=num_cpus::get() {
-        read_heavy(n).run::<ContrieTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
     println!("-- DashMap");
     for n in 1..=num_cpus::get() {
         read_heavy(n).run::<DashMapTable<u64>>();
-        gc_cycle();
-    }
-    println!("==");
-}
 
-fn rapid_grow_task() {
-    println!("== rapid grow");
-    println!("-- MutexStd");
-    for n in 1..=num_cpus::get() {
-        rapid_grow(n).run::<MutexStdTable<u64>>();
-        gc_cycle();
     }
     println!("");
-    println!("-- CHashMap");
+    println!("-- Shard");
     for n in 1..=num_cpus::get() {
-        rapid_grow(n).run::<CHashMapTable<u64>>();
-        gc_cycle();
+        read_heavy(n).run::<ShardTable<u64>>();
+
     }
-    println!("");
-    println!("-- Flurry");
-    for n in 1..=num_cpus::get() {
-        rapid_grow(n).run::<FlurryTable>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- Contrie");
-    for n in 1..=num_cpus::get() {
-        rapid_grow(n).run::<ContrieTable<u64>>();
-        gc_cycle();
-    }
-    println!("");
-    println!("-- DashMap");
-    for n in 1..=num_cpus::get() {
-        rapid_grow(n).run::<DashMapTable<u64>>();
-        gc_cycle();
-    }
-    println!("==");
 }
 
 fn main() {
     tracing_subscriber::fmt::init();
 
     cache_task();
-    exchange_task();
-    rapid_grow_task();
 }
