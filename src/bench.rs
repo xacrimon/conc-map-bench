25b4efc9d0097612f37c50a223e436439594945a
Joel Wejdenstål <jwejdenstal@icloud.com>
2023-08-21 18:41:49 +0200

    remove whitespace

--- src/bench.rs
+++ src/bench.rs
@@ -75,7 +75,7 @@ where
 
     let gen_threads = || {
         let n = num_cpus::get();
-        
+
         match n {
             0..=8 => (1..=n as u32).collect(),
             9..=16 => iter::once(1).chain((0..=n as u32).step_by(2).skip(1)).collect(),
